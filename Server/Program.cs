0bd69cfed81f2f7a76220acbaaa441c7da1b3858
Amir_Lynx <60301485+AmirLynx@users.noreply.github.com>
2021-10-28 14:37:18 +0330

    Add project files.

---
+++ Server/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server
+{
+    class Program
+    {
+        const int PORT_NO = 5000;
+        const string SERVER_IP = "127.0.0.1";
+
+        static void Main(string[] args)
+        {
+            //---listen at the specified IP and port no.---
+            IPAddress localAdd = IPAddress.Parse(SERVER_IP);
+            TcpListener listener = new TcpListener(localAdd, PORT_NO);
+            Console.WriteLine("Listening...");
+            listener.Start();
+
+            //---incoming client connected---
+            TcpClient client = listener.AcceptTcpClient();
+
+            //---get the incoming data through a network stream---
+            NetworkStream nwStream = client.GetStream();
+            byte[] buffer = new byte[client.ReceiveBufferSize];
+
+            //---read incoming stream---
+            int bytesRead = nwStream.Read(buffer, 0, client.ReceiveBufferSize);
+
+            //---convert the data received into a string---
+            string dataReceived = Encoding.ASCII.GetString(buffer, 0, bytesRead);
+            Console.WriteLine("Received : " + dataReceived);
+            Console.WriteLine("Write Your message to response :");
+            var response = Console.ReadLine();
+            //---write back the text to the client---
+            Console.WriteLine("Sending back : " + response);
+            var sendData = Encoding.ASCII.GetBytes(response);
+            nwStream.Write(sendData, 0, sendData.Length);
+            client.Close();
+            listener.Stop();
+            Console.ReadLine();
+        }
+    }
+}
