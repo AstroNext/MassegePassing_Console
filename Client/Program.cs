eea29342094878296cc01dc3c641d1ea7a737636
Amir_Lynx <60301485+AmirLynx@users.noreply.github.com>
2021-10-28 14:41:46 +0330

    fix somthing

--- Client/Program.cs
+++ Client/Program.cs
@@ -13,25 +13,27 @@ namespace Client
         const string SERVER_IP = "127.0.0.1";
         static void Main(string[] args)
         {
-            //---data to send to the server---
-            Console.WriteLine("Enter your text to send");
+            Console.Title = "Client";
+            Console.WriteLine("Enter your text to send : ");
             string textToSend = Console.ReadLine();
-
-            //---create a TCPClient object at the IP and port no.---
-            TcpClient client = new TcpClient(SERVER_IP, PORT_NO);
-            NetworkStream nwStream = client.GetStream();
-            byte[] bytesToSend = ASCIIEncoding.ASCII.GetBytes(textToSend);
-
-            //---send the text---
-            Console.WriteLine("Sending : " + textToSend);
-            nwStream.Write(bytesToSend, 0, bytesToSend.Length);
-
-            //---read back the text---
-            byte[] bytesToRead = new byte[client.ReceiveBufferSize];
-            int bytesRead = nwStream.Read(bytesToRead, 0, client.ReceiveBufferSize);
-            Console.WriteLine("Received : " + Encoding.ASCII.GetString(bytesToRead, 0, bytesRead));
-            Console.ReadLine();
-            client.Close();
+            try
+            {
+                TcpClient client = new TcpClient(SERVER_IP, PORT_NO);
+                Console.Title = "Client " + client.Client.Handle;
+                NetworkStream nwStream = client.GetStream();
+                byte[] bytesToSend = ASCIIEncoding.ASCII.GetBytes(textToSend);
+                Console.WriteLine("Sending : " + textToSend);
+                nwStream.Write(bytesToSend, 0, bytesToSend.Length);
+                byte[] bytesToRead = new byte[client.ReceiveBufferSize];
+                int bytesRead = nwStream.Read(bytesToRead, 0, client.ReceiveBufferSize);
+                Console.WriteLine("Received : " + Encoding.ASCII.GetString(bytesToRead, 0, bytesRead));
+                Console.ReadLine();
+                client.Close();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("ex : " + ex.Message);
+            }
         }
     }
 }
--- Server/Program.cs
+++ Server/Program.cs
@@ -12,32 +12,21 @@ namespace Server
     {
         const int PORT_NO = 5000;
         const string SERVER_IP = "127.0.0.1";
-
         static void Main(string[] args)
         {
-            //---listen at the specified IP and port no.---
             IPAddress localAdd = IPAddress.Parse(SERVER_IP);
             TcpListener listener = new TcpListener(localAdd, PORT_NO);
-            Console.WriteLine("Listening...");
+            Console.WriteLine("Listening ...");
             listener.Start();
-
-            //---incoming client connected---
             TcpClient client = listener.AcceptTcpClient();
-
-            //---get the incoming data through a network stream---
             NetworkStream nwStream = client.GetStream();
             byte[] buffer = new byte[client.ReceiveBufferSize];
-
-            //---read incoming stream---
             int bytesRead = nwStream.Read(buffer, 0, client.ReceiveBufferSize);
-
-            //---convert the data received into a string---
             string dataReceived = Encoding.ASCII.GetString(buffer, 0, bytesRead);
-            Console.WriteLine("Received : " + dataReceived);
+            Console.WriteLine("Received message from ( "+client.Client.Handle+" ) : " + dataReceived + "\n");
             Console.WriteLine("Write Your message to response :");
             var response = Console.ReadLine();
-            //---write back the text to the client---
-            Console.WriteLine("Sending back : " + response);
+            Console.WriteLine("Response : " + response);
             var sendData = Encoding.ASCII.GetBytes(response);
             nwStream.Write(sendData, 0, sendData.Length);
             client.Close();
